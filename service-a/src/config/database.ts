ed488fbb874d4c33f4fbb31ffabed3c675f77b14
Alii-Aydi <aliabdelkadersama@gmail.com>
2024-09-06 23:41:47 +0100

    fix rabbit v

--- service-a/src/config/database.ts
+++ service-a/src/config/database.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { join } from 'path';
+import { Message } from '../models/Message';
 const glob = require('glob');
 
 // Use glob to find all entity files in the models directory
 const entities = glob.sync(join(__dirname, '../models/*.ts')).map((file: string) => require(file).default);
+console.log(entities)
 
 export const AppDataSource = new DataSource({
     type: 'mongodb',
@@ -13,5 +15,5 @@ export const AppDataSource = new DataSource({
     useUnifiedTopology: true,
     synchronize: true,
     logging: true,
-    entities, // Dynamically include all entities
+    entities: [Message], // Dynamically include all entities
 });
--- service-a/src/index.ts
+++ service-a/src/index.ts
@@ -1,27 +1,43 @@
+import 'reflect-metadata'; // Ensure reflect-metadata is imported
 import './di/container'; // Ensure this import is at the top
 
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import { AppDataSource } from './config/database';
 import { RabbitMQ } from './rabbitmq/RabbitMQ';
 import messageRoutes from './routes/messageRoutes';
+import { container } from 'tsyringe'; // Import tsyringe container
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// Routes
 app.get("/", (req: Request, res: Response): void => {
     res.json("Hello You");
 });
 
 app.use('/api/messages', messageRoutes);
 
-AppDataSource.initialize().then(() => {
-    app.listen(3000, async () => {
+AppDataSource.initialize().then(async () => {
+    // Resolve RabbitMQ instance from the container
+    const rabbitMQ = container.resolve(RabbitMQ);
+
+    // Connect to RabbitMQ
+    await rabbitMQ.connect();
+
+    // Start the server
+    app.listen(3000, () => {
         console.log('Service-A running on port 3000');
-        await RabbitMQ.connect();
     });
+
+    // Handle process termination
     process.on('SIGINT', async () => {
-        await RabbitMQ.close();
+        await rabbitMQ.close();
+        process.exit(0);
     });
 }).catch((error: Error) => {
     console.error('Error initializing AppDataSource:', error);
